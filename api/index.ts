46254c74209ff19ff08fafddc9f32a0de47561e2
Pablo Benjamin Heredia Ruiz <pbheredia@udabol.edu.bo>
2025-12-16 14:53:08 -0400

    fix: use api folder handler for Vercel serverless

---
+++ api/index.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+let app: any;
+
+async function bootstrap() {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    
+    const config = new DocumentBuilder()
+      .setTitle('MdBot API')
+      .setDescription('API para el sistema MdBot')
+      .setVersion('1.0')
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+    
+    app.enableCors({
+      origin: [
+        'https://mdbotfront.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    });
+    
+    await app.init();
+  }
+  return app;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = await bootstrap();
+  const instance = app.getHttpAdapter().getInstance();
+  return instance(req, res);
+}
