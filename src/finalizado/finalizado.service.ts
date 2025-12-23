6a9790c1f66267b5e8d028a575c35a10982edab7
Benjamin Heredia <benjaherediaruiz@gmail.com>
2025-12-23 18:00:44 -0400

    add the califications

--- src/finalizado/finalizado.service.ts
+++ src/finalizado/finalizado.service.ts
@@ -2,9 +2,9 @@ import { Injectable, HttpException } from '@nestjs/common';
 import { PrismaService } from '../../utils/prisma.service';
 import { FinalizandoDto } from '../../utils/schemas/finalizando';
 @Injectable()
-export class FinalizandoService { 
-    constructor(private prisma: PrismaService) {}
-    async quejas (finalizandoDto: FinalizandoDto) {
+export class FinalizandoService {
+    constructor(private prisma: PrismaService) { }
+    async quejas(finalizandoDto: FinalizandoDto) {
         const quejas = await this.prisma.queja.findFirst({
             where: { id: finalizandoDto.id }
         });
@@ -15,11 +15,15 @@ export class FinalizandoService {
             where: { id: finalizandoDto.id },
             data: {
                 respuesta: finalizandoDto.descripcion
+            },
+            select: {
+                persona: true
             }
         })
+        const webhook = await fetch(process.env.URL_WEBHOOK, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ data: updateQuejas, tipo: 'queja', id_solicitud: finalizandoDto.id }) });
         return updateQuejas;
     }
-    async felicitaciones (finalizandoDto: FinalizandoDto) {
+    async felicitaciones(finalizandoDto: FinalizandoDto) {
         const felicitaciones = await this.prisma.felicitacion.findFirst({
             where: { id: finalizandoDto.id }
         });
@@ -30,11 +34,15 @@ export class FinalizandoService {
             where: { id: finalizandoDto.id },
             data: {
                 Respuesta: finalizandoDto.descripcion
+            },
+            select: {
+                persona: true
             }
         })
+        const webhook = await fetch(process.env.URL_WEBHOOK, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ data: updateFelicitaciones, tipo: 'felicitacion', id_solicitud: finalizandoDto.id }) });
         return updateFelicitaciones;
     }
-    async solicitudes (finalizandoDto: FinalizandoDto) {
+    async solicitudes(finalizandoDto: FinalizandoDto) {
         const solicitudes = await this.prisma.solicitud.findFirst({
             where: { id: finalizandoDto.id }
         });
@@ -45,8 +53,12 @@ export class FinalizandoService {
             where: { id: finalizandoDto.id },
             data: {
                 Respuesta: finalizandoDto.descripcion
+            },
+            select: {
+                persona: true
             }
-        })
+        });
+        const webhook = await fetch(process.env.URL_WEBHOOK, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ data: updateSolicitudes, tipo: 'solicitud', id_solicitud: finalizandoDto.id }) });
         return updateSolicitudes;
     }
 }
